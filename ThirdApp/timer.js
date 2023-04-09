bc9c7133697e9604644c5055729fdeaadeafacec
Kousiii <kousi291709@gmail.com>
2023-04-09 17:31:04 +0530

    Assignment

--- ThirdApp/timer.js
+++ ThirdApp/timer.js
@@ -5,10 +5,13 @@ var seconds= 0;
 var run=0;
 var pause=0;
 // console.log(hours+":"+minutes+":"+seconds);
+
 function start(){
+    document.getElementById("timer").style.display="block";
     run++;
     if(run==1){
         setInterval(function(){
+            
             document.getElementById("timer").innerHTML=String(hours).padStart(2, '0')+":"+String(minutes).padStart(2, '0')+":"+String(seconds).padStart(2, '0');
             if(seconds==60){
                 minutes++;
