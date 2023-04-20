e500f570c270ac9c3805376aea712931393ac706
Kousiii <kousi291709@gmail.com>
2023-04-20 21:44:41 +0530

    assignment

---
+++ ANGULAR/assignment10/src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  productlist:any;
+      constructor(private ps:ProductService){
+        this.ps.getOrder().subscribe(
+          {
+            next: (data:any)=> this.productlist =data,
+            error: ()=> this.productlist=[]
+          }
+        )
+      }
+
+}
